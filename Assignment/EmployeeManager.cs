9ac033fb7e55234781f985b7842c637055111c47
JayAkagi <b019782k@gmail.com>
2023-12-01 05:06:15 +0000

    Item Manager C#

---
+++ Assignment/EmployeeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Assignment
+{
+    public class EmployeeManager
+    {
+        private Dictionary<string, Employee> employees;
+
+        public EmployeeManager()
+        {
+            this.employees = new Dictionary<string, Employee>();
+        }
+
+        public void AddEmployee(Employee e)
+        {
+            employees.Add(e.EmpName, e);
+        }
+
+        public Employee FindEmployee(string EmpName)
+        {
+            try
+            {
+                return employees[EmpName];
+            }
+            catch (KeyNotFoundException)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Assignment/TransactionLogManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Assignment
+{
+    public class TransactionLogManager
+    {
+        private List<TransactionLogEntry> transactions;
+
+        public TransactionLogManager()
+        {
+            this.transactions = new List<TransactionLogEntry>();
+        }
+
+        public void AddTransactionLog(TransactionLogEntry entry)
+        {
+            transactions.Add(entry);
+        }
+
+        public List<TransactionLogEntry> GetTransactionLog()
+        {
+            return transactions;
+        }
+    }
+}
